1b1ef75fb4eadf70e349964dbce80507dc28e839
Michael Tzimourakas <michael.tzimourakas@gmail.com>
2021-02-14 17:32:11 -0500

    Add files via upload
    add function to read docx files

--- mitre_natural_language_processor.py
+++ mitre_natural_language_processor.py
@@ -1,6 +1,41 @@
 # Import packages
-import os, datetime, nltk, docx, pptx, PyPDF2
+import os, datetime, nltk, docx, pptx, PyPDF2, sys, subprocess, tkinter as tk
 
+from tkinter.filedialog import askopenfilename
+
+
+# Datasets
+
+raw_sentences = []
+
+def read_file(filename):
+    doc = docx.Document(filename)
+    for line in doc.paragraphs:
+        #print(line.text)
+        raw_sentences.append(line.text)
+
+
+def main():
+
+    filename = 'C:\\Users\\Michael\\Documents\\GMU\\SYST 699 Masters Project\\GMU-699-Masters-Project\\Tzimourakas_Paper_Review1.docx'
+    #if len(sys.argv) !=2:
+        # Open gui to select file
+    #    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    #else:
+        # Get file
+    #    filename = str(sys.argv[1])
+    
+    print(filename)
+    read_file(filename)
+
+
+    processed_sentences = list(filter(None, raw_sentences))
+    for sentance in processed_sentences:
+        print(sentance)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
